33c79958921a068be41e121c516846b42d12ce0f
Myhannad Alsrahen <mohanadalsrahenm1029@gmail.com>
2024-02-10 06:41:47 -0800

    complete the project put there is some bugs needs to fix

--- main.py
+++ main.py
@@ -12,23 +12,47 @@ for number in range(0, 2):
 your_score = your_choice[0] + your_choice[1]
 computer_score = computer_choice[0] + your_choice[1]
 
-print(computer_choice[0])
-print(f"your cards is :{your_choice} and your score is :{your_score}")
+print(f"one of computer card: {computer_choice[0]}")
+print(f"your cards is: {your_choice} and your score is: {your_score}")
 
 # print ("hit ", "stand")
 
 draw_card = input("if you want an extra card type 'hit' type 'stand' to stop: ")
 
-while draw_card == "hit" and your_score <21:
-  extra_card = random.choice(cards)
-  your_choice.append(extra_card)
-  your_score += extra_card
-  print(f"your cards is :{your_choice} and your score is :{your_score}")
-  if your_score > 21:
-      print("you bust")
-  else:
-      draw_card = input("if you want an extra card type 'hit' type'stand' to stop: ")
+while your_score < 21:
+    extra_card = random.choice(cards)
+    your_choice.append(extra_card)
+    your_score += extra_card
+    print(f"your cards is :{your_choice} and your score is :{your_score}")
+    if your_score > 21:
+        print("you bust")
+    else:
+        draw_card = input("if you want an extra card type 'hit' type'stand' to stop: ")
 
-# if draw_card == "stand":
-#     print(f"computer cards is :{computer_choice} and your score is :{computer_score}")
 
+if draw_card == "stand":
+    print(f"your cards is :{your_choice} and your score is :{your_score}")
+    print(f"computer cards is :{computer_choice} and the score is :{computer_score}")
+
+    if computer_score > your_score:
+        print("you lose")
+
+    else:
+        should_continue = True
+        while should_continue:
+            if computer_score < 17:
+                extra_card_computer = random.choice(cards)
+                computer_choice.append(extra_card_computer)
+                computer_score += extra_card_computer
+                print(
+                    f"computer cards is :{computer_choice} and the score is :{computer_score}"
+                )
+                if computer_score > 21:
+                    print("you win")
+                    should_continue = False
+                elif your_score < computer_score:
+                    print("you lose")
+                    should_continue = False
+            if computer_score >= 17 and your_score > computer_score:
+                print("you win")
+                should_continue = False
